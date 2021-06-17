27b9206b4f7cfe4798fa844ddd2a223fea545d53
sfky <meaple.sfky@icloud.com>
2021-06-17 11:56:49 +0800

    oslab

---
+++ OSInterface/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  OSInterface
+//
+//  Created by sfky on 2021/6/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
